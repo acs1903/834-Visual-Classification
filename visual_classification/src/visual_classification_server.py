46ffe6c546c0fe0b16394f911595ec2a5cab1bd5
student <alicansoylemezoglu@gmail.com>
2016-05-08 21:16:11 -0400

    added visual_classification package

--- visual_classification/src/visual_classification_server.py
+++ visual_classification/src/visual_classification_server.py
@@ -2,32 +2,44 @@
 
 import rospy
 import actionlib
+import numpy as np
+import cv2
 
+from sensor_msgs.msg import CompressedImage
 from visual_classification.msg import VisualClassificationAction, VisualClassificationGoal, VisualClassificationResult, VisualClassificationFeedback
 
-class Activity(object):
-    def __init__(self, activity_name='activity_name'):
+class VisualClassificationActivity(object):
+    def __init__(self, activity_name='visual_classification'):
+
+        rospy.Subscriber("/axis1", CompressedImage,self.image_callback, queue_size = 1)
+
         self.action_server = actionlib.SimpleActionServer(activity_name, VisualClassificationAction, self.execute_action, False)
         self.action_server.start()
+
+    def image_callback(self,image):
+        self.image_data = image.data
 
     def execute_action(self, goal):
         # YOUR CODE HERE! Replace this method.
         rospy.loginfo("Action called with input: {}".format(goal.input))
 
 
-        #rospy.sleep(rospy.Duration(1.0))
-
-
-
         # This is how you can send feedback to the client
         feedback = VisualClassificationFeedback()
-        feedback.percent_complete = 0.5
+        feedback.current_step = "starting to convert from CompressedImage to Numpy Array"
         self.action_server.publish_feedback(feedback)
 
+        image = self.image_data
+        np_arr = np.fromstring(image, np.uint8)
+        image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
 
+        feedback = VisualClassificationFeedback()
+        feedback.current_step = "finished converting from CompressedImage to Numpy Array"
+        self.action_server.publish_feedback(feedback)
 
-        #rospy.sleep(rospy.Duration(1.0))
-
+        feedback = VisualClassificationFeedback()
+        feedback.current_step = "classifying image"
+        self.action_server.publish_feedback(feedback)
 
         # Before you exit, be sure to succeed or fail.
         classification = cnn_function(image_format) ##TO DO: FIGURE THIS OUT
@@ -35,10 +47,8 @@ class Activity(object):
         result.output = classification #"I'm done!" # Put your output here
         self.action_server.set_succeeded(result)
 
-
-
 if __name__ == '__main__':
     rospy.init_node('visual_classification_server')
-    activity = Activity(activity_name='image_classifcation')
+    activity = VisualClassificationActivity()
     rospy.loginfo('Visual classifier running')
     rospy.spin()
